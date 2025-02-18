ddf07db1fab6a5e7f91ab10ed249b6a6db1f7581
yumeng <yzha5882@uwo.ca>
2025-02-17 21:39:46 -0500

    init

---
+++ run_server.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd "$SCRIPT_DIR"
+
+cd ./server
+
+if command -v docker &> /dev/null; then
+    if command -v docker-compose &> /dev/null; then
+        docker-compose up -d
+        echo "Docker start"
+    fi
+fi
+
---
+++ server/src/main/java/com/hacking/Server.java
@@ -0,0 +1,21 @@
+package com.hacking;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import static spark.Spark.*;
+
+public class Server {
+    private static final Logger logger = LogManager.getLogger(Server.class);
+
+    public static void main(String[] args) {
+        port(8080);
+
+        get("/", (req, res) -> {
+            String userAgent = req.headers("User-Agent");
+            logger.error("Received User-Agent: " + userAgent);
+            return "Hello, world!";
+        });
+
+        System.out.println("server is running on port 8080...");
+    }
+}
